bbb91a4cddfc8dc7518193098cd2609172d0ab46
JayHaymes <jjhaymes23@gmail.com>
2024-03-30 16:27:33 -0500

    updated server.js & created react client

--- server/server.js
+++ server/server.js
@@ -12,57 +12,99 @@ app.use(express.json());
 //ROUTES
 // get all products
 app.get("/api/v1/products", async (req, res) => {
-   const result = await db.query("SELECT * FROM products");
-   console.log(result)
-    // console.log(req.body)
-    res.json({
-        status: "success",
-        data: {
-            product: ["Chair", "Mattress", "Bed Frame", "Couch"]
-        }
-    })
+    try {
+        const result = await db.query("SELECT * FROM products");
+        res.json({
+            status: "success",
+            results: result.rows.length,
+            data: {
+                products: result.rows
+            }
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
+
 });
 
 //get single product
-app.get("/api/v1/products/:id", (req, res) => {
-    // console.log(req.params);
-    // console.log(req.body)
-    res.status(200).json({
-        status: "success",
-        data: {
-            product: "Chair"
-        }
-    })
+app.get("/api/v1/products/:id", async (req, res) => {
+    try {
+        const paramsId = req.params.id;
+        const result = await db.query('SELECT * FROM products WHERE product_id = $1',
+            [
+                paramsId
+            ]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                product: result.rows[0]
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 //create a product
-app.post("/api/v1/products", (req, res) => {
-    // console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data: {
-            product: "Chair"
-        }
-    })
+app.post("/api/v1/products", async (req, res) => {
+    try {
+        const result = await db.query("INSERT INTO products (name,description,price,stock_quantity) VALUES ($1, $2, $3, $4) RETURNING *",
+            [
+                req.body.name,
+                req.body.description,
+                req.body.price,
+                req.body.stock_quantity
+            ]);
+        res.status(201).json({
+            status: "success",
+            data: {
+                product: result.rows[0]
+            }
+        });
+    } catch (error) {
+        console.error(error)
+    }
 });
 
 //update a product
-app.put("/api/v1/products/:id", (req, res) => {
-    // console.log(req.params.id)
-    // console.log(req.body)
-    res.status(200).json({
-        status: "success",
-        data: {
-            product: "Chair"
-        }
-    })
+app.put("/api/v1/products/:id", async (req, res) => {
+    try {
+        const result = await db.query("UPDATE products SET name = $1, description = $2, price = $3, stock_quantity = $4 WHERE product_id = $5 RETURNING *",
+            [
+                req.body.name,
+                req.body.description,
+                req.body.price,
+                req.body.stock_quantity,
+                req.params.id
+            ]);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                product: result.rows[0]
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 //delete a product
-app.delete("/api/v1/products/:id", (req,res) => {
-    res.status(204).json({
-        status: "success"
-    });
+app.delete("/api/v1/products/:id", async (req, res) => {
+    try {
+        const result = await db.query("DELETE FROM products WHERE product_id = $1",
+            [
+                req.params.id
+            ]);
+        res.status(204).json({
+            status: "success",
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
 });
 
 port = process.env.PORT || 3000
