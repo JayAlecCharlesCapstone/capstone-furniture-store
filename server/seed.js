544f3af60b3a0e14b38f29c4d732a51aa9ba4ed7
JayHaymes <jjhaymes23@gmail.com>
2024-04-05 18:32:45 -0500

    fixed middleware JWT token

--- server/seed.js
+++ server/seed.js
@@ -39,14 +39,16 @@ async function createTables() {
 
         CREATE TABLE Customers (
             customer_id SERIAL PRIMARY KEY,
-            username VARCHAR(100) NOT NULL UNIQUE,
-            password VARCHAR(100) NOT NULL,
             name VARCHAR(100) NOT NULL,
             email VARCHAR(100) NOT NULL UNIQUE,
             phone VARCHAR(20),
+            username VARCHAR(50) NOT NULL UNIQUE,
+            password_hash VARCHAR(255),
+            password_salt VARCHAR(255),
             address_id INT,
             FOREIGN KEY (address_id) REFERENCES Addresses(address_id)
         );
+        
 
         CREATE TABLE Products (
             product_id SERIAL PRIMARY KEY,
