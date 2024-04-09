d293d584185d2231da3a0191ca350cd740a6c148
AlecWilliams00 <alecw89@gmail.com>
2024-04-08 19:55:40 -0500

    set up route for product details

--- client/src/routes/productdetails.jsx
+++ client/src/routes/productdetails.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
-export const productdetails = () => {
-  return (
-    <div>productdetails</div>
+export default function ProductDetails(){
+  const{productId} = useParams();
+  const [product, setProduct] = useState(null)
+
+
+  useEffect(()=>{
+    const fetchProduct = async() =>{
+      try{
+        const response = await fetch(`http://localhost:3000/api/v1/products/${productId}`);
+        if (!response.ok){
+          throw new Error('failed to fetch prodcut')
+        }
+        const result = await response.json();
+
+        setProduct(result.product);
+      }catch(error){
+        console.error(error);
+      }
+    };
+
+    fetchProduct()
+  }, [productId])
+
+  return(
+    <>
+    <div id = 'productDetails'>
+      <h2>Product Details</h2>
+      {product&& (
+        <ul className='singleProductDetails'>
+          <li>Name: {product.name}</li>
+          <li>Description: {product.description}</li>
+          <li>Price: {product.price}</li>
+          <li>Quantity: {product.quantity}</li>
+          <li><img src = {product.coverimage} alt = {product.name}></img> </li>
+        </ul>
+      )}
+    </div>
+    </>
   )
 }
