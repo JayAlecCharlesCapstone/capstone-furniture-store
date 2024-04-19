af3361d8ada73557a0c38cb536f52fe154cb6827
JayHaymes <jjhaymes23@gmail.com>
2024-04-18 19:19:25 -0500

    initial branch creation

--- client/src/routes/Account.jsx
+++ client/src/routes/Account.jsx
@@ -3,38 +3,37 @@ import React, { useState, useEffect } from 'react';
 export default function Account({ token }) {
   const [customer, setCustomer] = useState(null);
   const [cart, setCart] = useState(null);
-
+console.log(customer)
   async function fetchCustomer() {
     try {
-      const response = await fetch("localhost:3000/api/v1/customer", {
+      const response = await fetch("http://localhost:3000/api/v1/customer", {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          'Content-Type': 'application/json'
         }
       });
       if (!response.ok) {
         throw new Error('Failed to fetch customer information');
       }
       const result = await response.json();
-      setCustomer(result);
+      setCustomer(result.data.customers);
+      console.log(result)
     } catch (error) {
       console.error(error);
     }
   }
-
+  
   async function fetchCart() {
     try {
-      const response = await fetch("localhost:3000/api/v1/customer", {
+      const response = await fetch("http://localhost:3000/api/v1/cart", {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`
         }
       });
       if (!response.ok) {
-        throw new Error('Failed to fetch customer information');
+        throw new Error('Failed to fetch cart information');
       }
       const result = await response.json();
-      console.log(token)
       setCart(result);
     } catch (error) {
       console.error(error);
@@ -44,7 +43,7 @@ export default function Account({ token }) {
   useEffect(() => {
     if (token) {
       fetchCustomer();
-      fetchCart(); // You may need to call fetchCart here if you're fetching cart information separately
+      fetchCart();
     }
   }, [token]);
 
@@ -61,7 +60,7 @@ export default function Account({ token }) {
       if (!response.ok) {
         throw new Error('Failed to return product');
       }
-      fetchCart(); // Fetch updated cart information after returning the product
+      fetchCart(); 
     } catch (error) {
       console.error(error);
     }
@@ -73,7 +72,7 @@ export default function Account({ token }) {
         {customer && (
           <>
             <h1>Customer Login Information:</h1>
-            <h3>First Name:</h3> <p>{customer.firstname}</p>
+            <h3>Name:</h3> <p>{customer[0].name}</p>
             <h3>Last Name:</h3> <p>{customer.lastname}</p>
             <h3>Email:</h3> <p>{customer.email}</p>
           </>
