37cc052e8e5dc4d93866cabdc7dede530541ce73
JayHaymes <jjhaymes23@gmail.com>
2024-04-23 17:55:20 -0500

    initial orders commit

---
+++ client/src/routes/Orders.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function Orders({token}) {
+    const [customer, setCustomer] = useState(null);
+    const [car, setCart] = useState([]);
+
+
+    const createOrder = async () => {
+        
+    }
+
+
+
+}
