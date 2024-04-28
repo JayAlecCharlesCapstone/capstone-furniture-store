4db94e621105999b8d5ed8e3f93e9b1cedb4d1ad
JayHaymes <jjhaymes23@gmail.com>
2024-04-28 17:46:56 -0500

    .

--- client/src/routes/AddProduct.jsx
+++ client/src/routes/AddProduct.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
+
 function AddProduct({ token }) {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState('');
     const [stock_quantity, setStock_quantity] = useState('');
+    const [image_url, setImage_url] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -19,7 +21,8 @@ function AddProduct({ token }) {
                     name,
                     price,
                     description,
-                    stock_quantity
+                    stock_quantity,
+                    image_url
                 })
             });
 
@@ -33,50 +36,73 @@ function AddProduct({ token }) {
             setPrice('');
             setDescription('');
             setStock_quantity('');
+            setImage_url('');
         } catch (error) {
             console.error('Error adding product:', error.message);
             alert('Failed to add product. Please try again.');
         }
     };
 
-
     return (
-        <div>
+        <div className="add-product-container">
             <h2 className="add-product-header">Add New Product</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
+            <form onSubmit={handleSubmit} className="add-product-form">
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
 
-                <label>Price:</label>
-                <input
-                    type="number"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                    required
-                />
+                <div className="form-group">
+                    <label htmlFor="price">Price:</label>
+                    <input
+                        type="number"
+                        id="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        required
+                    />
+                </div>
 
-                <label>Description:</label>
-                <input
-                    type="text"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    required
-                />
+                <div className="form-group">
+                    <label htmlFor="description">Description:</label>
+                    <input
+                        type="text"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                    />
+                </div>
 
-                <label>Quantity:</label>
-                <input
-                    type="number"
-                    value={stock_quantity}
-                    onChange={(e) => setStock_quantity(e.target.value)}
-                    required
-                />
+                <div className="form-group">
+                    <label htmlFor="quantity">Quantity:</label>
+                    <input
+                        type="number"
+                        id="quantity"
+                        value={stock_quantity}
+                        onChange={(e) => setStock_quantity(e.target.value)}
+                        required
+                    />
+                </div>
 
-                <button className='addProductButton' type="submit">Add Product</button>
+                <div className="form-group">
+                    <label htmlFor="image">Image URL:</label>
+                    <input
+                        type="text"
+                        id="image"
+                        value={image_url}
+                        onChange={(e) => setImage_url(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <button className="add-product-button" type="submit">Add Product</button>
             </form>
         </div>
     );
